be3c4e5e696f2396c05f554e9a76fa7bd1861536
Hüseyin Kaplan <huseyinkapln0@gmail.com>
2023-07-16 00:42:21 +0300

    Para ekleme eklendi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QInputDialog
 from anaTasarim import Ui_MainWindow
 import requests
 import json
@@ -25,6 +25,8 @@ class Window(QtWidgets.QMainWindow):
         self.ui.sbBozdur.textChanged.connect(self.Hesap)
         
         self.ui.btnOnay.clicked.connect(self.Onay)
+        
+        self.ui.btnParaEkle.clicked.connect(self.ParaEkleme)
     
     
     def Hesap(self, text):
@@ -75,10 +77,30 @@ class Window(QtWidgets.QMainWindow):
             else:
                 item.setText(f"{(1 / (apiJson['conversion_rates'][kurlar[kurIndex]])):.4f} TRY")
                 kurIndex += 1
-                         
+                
+    
+    def ParaEkleme(self):
+        miktar, ok = QInputDialog.getInt(self, "Para Ekleme", "Eklemek istediğiniz tutarı giriniz:\n(tek seferde en fazla 100.000 birim eklenebilir)", 0, 0, 100000, 1)
+        
+        if ok and miktar is not None:
+            cins, ok = QInputDialog.getItem(self, "Para Ekleme", "Eklemek istediğiniz döviz cinsini seçiniz: ", ['TRY', 'USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'SEK', 'KRW', 'NOK', 'NZD', 'INR', 'MXN', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CDF', 'CLP', 'CNY', 'COP', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'FJD', 'FKP', 'FOK', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NPR', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'UYU', 'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL'])
+                   
+        if ok and cins is not None:
+            msg = QMessageBox()
+        
+            msg.setWindowTitle("İşlem Onayı")
+            msg.setText("İşleminizi Onaylıyor musunuz?")
+            
+            msg.setIcon(QMessageBox.Question)
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msg.setDefaultButton(QMessageBox.Ok)
+            
+            msg.setDetailedText(f"Hesabınıza {miktar} {cins} eklenecektir.")
+            
+            msg.exec_()
+   
         
 def app(apiKey):
-    apiKey = apiKey
     app = QtWidgets.QApplication(sys.argv)
     win = Window(apiKey)
     win.show()
@@ -89,6 +111,5 @@ def main():
     apiKey = str(input("'https://www.exchangerate-api.com' sitesi için API anahtarınızı giriniz: "))
     app(apiKey) 
     
-
-
+    
 main()
